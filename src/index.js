ca1f2a66f714b7463528124e69d07dae8e0a18dc
ldh7228 <ldh7228@gmail.com>
2021-07-24 18:18:56 +0900

    Add keydown animation

--- src/index.js
+++ src/index.js
@@ -1,72 +1,107 @@
-function sound(event) {
+const Do = document.getElementById('C');
+const DoSharp = document.getElementById('Csharp');
+const Re = document.getElementById('D');
+const ReSharp = document.getElementById('Dsharp');
+const Mi = document.getElementById('E');
+const Fa = document.getElementById('F');
+const FaSharp = document.getElementById('Fsharp');
+const So = document.getElementById('G');
+const SoSharp = document.getElementById('Gsharp');
+const La = document.getElementById('A');
+const LaSharp = document.getElementById('Asharp');
+const Ti = document.getElementById('B');
+const highDo = document.getElementById('highC');
+const highDoSharp = document.getElementById('highCsharp');
+const highRe = document.getElementById('highD');
+const highReSharp = document.getElementById('highDsharp');
+const highMi = document.getElementById('highE');
+
+function soundStart(event) {
     if (event.key == 'a') {
         let music = new Audio('../asset/do.wav');
+        Do.classList.add("decrease");
         music.play();
     }
     if (event.key == 'w') {
         let music = new Audio('../asset/doSharp.wav');
+        DoSharp.classList.add("decrease");
         music.play();
     }
     if (event.key == 's') {
         let music = new Audio('../asset/re.wav');
+        Re.classList.add("decrease");
         music.play();
     }
     if (event.key == 'e') {
         let music = new Audio('../asset/reSharp.wav');
+        ReSharp.classList.add("decrease");
         music.play();
     }
     if (event.key == 'd') {
         let music = new Audio('../asset/mi.wav');
+        Mi.classList.add("decrease");
         music.play();
     }
     if (event.key == 'f') {
         let music = new Audio('../asset/fa.wav');
+        Fa.classList.add("decrease");
         music.play();
     }
     if (event.key == 't') {
         let music = new Audio('../asset/faSharp.wav');
+        FaSharp.classList.add("decrease");
         music.play();
     }
     if (event.key == 'g') {
         let music = new Audio('../asset/so.wav');
+        So.classList.add("decrease");
         music.play();
     }
     if (event.key == 'y') {
         let music = new Audio('../asset/soSharp.wav');
+        SoSharp.classList.add("decrease");
         music.play();
     }
     if (event.key == 'h') {
         let music = new Audio('../asset/la.wav');
+        La.classList.add("decrease");
         music.play();
     }
     if (event.key == 'u') {
         let music = new Audio('../asset/laSharp.wav');
+        LaSharp.classList.add("decrease");
         music.play();
     }
     if (event.key == 'j') {
         let music = new Audio('../asset/ti.wav');
+        Ti.classList.add("decrease");
         music.play();
     }
     if (event.key == 'k') {
         let music = new Audio('../asset/highDo.wav');
+        highDo.classList.add("decrease");
         music.play();
     }
     if (event.key == 'o') {
         let music = new Audio('../asset/highDoSharp.wav');
+        highDoSharp.classList.add("decrease");
         music.play();
     }
     if (event.key == 'l') {
         let music = new Audio('../asset/highRe.wav');
+        highRe.classList.add("decrease");
         music.play();
     }
     if (event.key == 'p') {
         let music = new Audio('../asset/highReSharp.wav');
+        highReSharp.classList.add("decrease");
         music.play();
     }
     if (event.key == ';') {
         let music = new Audio('../asset/highMi.wav');
+        highMi.classList.add("decrease");
         music.play();
     }
 }
 
-document.addEventListener('keydown', sound);
+document.addEventListener('keydown', soundStart);
