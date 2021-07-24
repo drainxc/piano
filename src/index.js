e9f0f1a8a2e9f04f394f7c902d2493cb0fffb722
ldh7228 <ldh7228@gmail.com>
2021-07-24 17:23:43 +0900

    Add high note

--- src/index.js
+++ src/index.js
@@ -1,8 +1,32 @@
-function start(event) {
+function sound(event) {
     if (event.key == 'a') {
-        let music = new Audio('');
+        let music = new Audio('../asset/do.wav');
+        music.play();
+    }
+    if (event.key == 's') {
+        let music = new Audio('../asset/re.wav');
+        music.play();
+    }
+    if (event.key == 'd') {
+        let music = new Audio('../asset/mi.wav');
+        music.play();
+    }
+    if (event.key == 'f') {
+        let music = new Audio('../asset/fa.wav');
+        music.play();
+    }
+    if (event.key == 'g') {
+        let music = new Audio('../asset/so.wav');
+        music.play();
+    }
+    if (event.key == 'h') {
+        let music = new Audio('../asset/la.wav');
+        music.play();
+    }
+    if (event.key == 'j') {
+        let music = new Audio('../asset/ti.wav');
         music.play();
     }
 }
 
-document.addEventListener('keydown', start);
+document.addEventListener('keydown', sound);
